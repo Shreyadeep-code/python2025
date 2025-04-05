d8abc22c88ca88b7f05048c19cbb4f53e01dee9a
Shreyadeep-code <shreyadeepparida@gmail.com>
2025-04-05 19:30:46 +0530

    movie list

---
+++ enterthreemovies.py
@@ -0,0 +1,8 @@
+movies = []
+mov = input("enter the ist movie")
+movies.append(mov)
+mov =input("enter second movie")
+movies.append(mov)
+mov = input("enter 3rd movie")
+movies.append(mov)
+print(movies)
